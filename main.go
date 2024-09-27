18ee73c1d3c03839c4cd483c3e79dbf866cdadf3
JeanRibes <jean@ribes.ovh>
2024-09-27 19:15:25 +0200

    pas de dependance

--- main.go
+++ main.go
@@ -5,15 +5,10 @@ import (
 	"log"
 	"os"
 	"strconv"
-
-	//"golang.org/x/sys/unix"
-	unix "syscall"
+	"syscall"
 )
 
 func main() {
-	/*if err := syscall.Setuid(0); err != nil {
-		log.Fatal(err)
-	}*/
 	if os.Args[1] == "mount" {
 		device := os.Args[2]
 		path := os.Args[3]
@@ -25,19 +20,18 @@ func main() {
 			if err != nil {
 				log.Fatal(err)
 			}
-			if err := unix.Mount(device, path, "nilfs2", unix.MS_RDONLY|unix.MS_NOATIME|unix.MS_ASYNC, fmt.Sprintf("discard,cp=%d", cp)); err != nil {
+			if err := syscall.Mount(device, path, "nilfs2", syscall.MS_RDONLY|syscall.MS_NOATIME|syscall.MS_ASYNC, fmt.Sprintf("discard,cp=%d", cp)); err != nil {
 				log.Fatal(err)
 			}
 		} else {
-			if err := unix.Mount(device, path, "nilfs2", unix.MS_NOATIME|unix.MS_ASYNC, "discard"); err != nil {
+			if err := syscall.Mount(device, path, "nilfs2", syscall.MS_NOATIME|syscall.MS_ASYNC, "discard"); err != nil {
 				log.Fatal(err)
 			}
 		}
-
 	} else {
 		for _, path := range os.Args[2:] {
 			log.Println("unmount", path)
-			if err := unix.Unmount(path, unix.MNT_DETACH); err != nil {
+			if err := syscall.Unmount(path, syscall.MNT_DETACH); err != nil {
 				log.Print(err)
 			}
 		}
